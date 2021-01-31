f04ddc892700e62cedec28be40b154949fa9960e
Ro5hi <slen.detablan@gmail.com>
2021-01-31 10:45:36 -0700

    defined controllers build & parts with serializer

--- rails_api/app/controllers/api/v1/builds_controller.rb
+++ rails_api/app/controllers/api/v1/builds_controller.rb
@@ -1,18 +1,31 @@
 class API::V1::BuildsController < ApplicationController
 
     def index
+        @build = Build.all 
     end 
 
     def show 
+        render json: BuildSerializer.new(@build), status: :ok
     end 
 
     def create 
+        @build = Build.build(create_params)
+        if @build.save 
+            render json: BuildSerializer.new(@build), status: :ok
+        else 
+            @build == 0 
+            render json: @build.error 
+        end 
     end 
+    
+    private 
 
-    def update 
-    end 
+    def create_params 
+        params.require(:build).permit(:parts, :description)
+    end
 
-    def destroy
-    end 
+    def set_build
+        @build = Build.find_by(id: params[:id])
+    end
 
 end
--- rails_api/app/controllers/api/v1/parts_controller.rb
+++ rails_api/app/controllers/api/v1/parts_controller.rb
@@ -1,9 +1,12 @@
 class API::V1::PartsController < ApplicationController
 
     def index
+        @parts = Part.all 
+        render json: PartSerializer.new(@parts), status: :ok
     end 
 
     def show 
+        render json: PartSerializer.new(@part), status: :ok 
     end 
 
 end
