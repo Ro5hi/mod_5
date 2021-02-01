8fbc0b0dccee79526c9d9b0e4772277314070d2c
Ro5hi <slen.detablan@gmail.com>
2021-02-01 14:09:18 -0700

    removed unused connect, added line for extra navlinks

--- react-redux/rigger/src/components/presentational/Nav.js
+++ react-redux/rigger/src/components/presentational/Nav.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 
 export default class Nav extends Component {
@@ -10,7 +9,8 @@ export default class Nav extends Component {
                 <ul><NavLink exact to="/addpart">Add Part</NavLink></ul>
                 <ul><NavLink exact to="/builds">Builds List</NavLink></ul>
                 <ul><NavLink exact to="/newbuild">New Build</NavLink></ul>
-
+                // navigational bar links go here if needed
             </div>
+        )
     }
 }
