3dbf19c5bb78102ab9e5d1749a8066022ba5167d
Ro5hi <slen.detablan@gmail.com>
2021-02-02 11:36:22 -0700

    change route, debugging post error

--- rails_api/config/routes.rb
+++ rails_api/config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  post "/api/v1/addpart", to: "api/v1/parts#create"
-  post "/api/v1/newbuild", to: "api/v1/builds#create"
+  post "/api/v1/addpart/:id", to: "api/v1/parts#create"
+  post "/api/v1/newbuild/:id", to: "api/v1/builds#create"
+  delete "/api/v1/part/:id", to: "api/v1/parts#destroy"
   get "/api/v1/builds", to: "api/v1/builds#index"
   get "/api/v1/build/:id", to: "api/v1/build#show"
   get "/api/v1/parts", to: "api/v1/parts#index"
+  get "/api/v1/parts/:id", to: "api/v1/parts#show"
   
   #changed routes
 
